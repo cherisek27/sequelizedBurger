611a9d312bd604dac5a10f97a98d6432784ddfbf
cherisek27 <cheriseknox27@gmail.com>
2017-09-17 11:56:14 -0700

    delete seeds.sql

--- db/seeds.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO burgers (burger_name, devoured)
-	VALUES ('bacon cheeseburger', FALSE);
-INSERT INTO burgers (burger_name, devoured)
-	VALUES ('turkey burger', FALSE); 
-INSERT INTO burgers (burger_name, devoured) 
-	VALUES ('veggie burger', FALSE);
-INSERT INTO burgers (burger_name, devoured) 
-	VALUES ('double patty burger', FALSE);
